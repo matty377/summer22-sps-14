7b507b18600b528000eec38861de62878ae2982e
Truc <trucdg@bu.edu>
2022-06-27 22:40:07 +0000

    add a map with a marker center at Google Headquarter as default

--- group-project/src/main/webapp/script.js
+++ group-project/src/main/webapp/script.js
@@ -13,3 +13,29 @@
 // limitations under the License.
 
 
+let map;
+
+//initMap(): is a callback function,
+//           will be executed after the Google Map API Async Script loads
+
+function initMap(){
+
+    //default location is Google headquarter
+    let location = {
+        lat: 37.419857,
+        lng: -122.078827
+    };
+
+    let mapOptions = {
+        center: location,
+        zoom: 9
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    let marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+
+}
